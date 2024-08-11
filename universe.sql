d9021d6aa5e82f915bfa5bbd6b14c5c41c5659fc
FFern97 <ffernanndez97@gmail.com>
2024-08-11 01:09:49 -0300

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -1,1 +1,16 @@
+logear cuenta freecodecamp en postgres
 
+crear DB universe
+
+Conectarla
+
+
+Crear tabla galaxy 
+
+
+DESPUES SIGUE ESTA TABLA STAR. 
+
+
+
+postgres=> CREATE TABLE star (star_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, age_in_million_years NUMERIC, size INT NOT NULL, mass I
+NT, galaxy_id INT, CONSTRAINT fk_galaxy FOREING KEY(galaxy_id) REFERENCES galaxy(galaxy_id));NSTRAINT fk_galaxy FOREING KEY(galaxy_id) REFERENCES galaxy(galaxy_id));
