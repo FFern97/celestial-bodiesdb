9e36c25459fcd263bca7255582e0bfd60e5bf242
FFern97 <ffernanndez97@gmail.com>
2024-08-12 13:04:42 -0300

    Update universe.sql

--- universe.sql
+++ universe.sql
@@ -14,5 +14,8 @@ CONSTRAINT fk_galaxy FOREING KEY(galaxy_id) REFERENCES galaxy(galaxy_id));
 postgres=> CREATE TABLE planet (planet_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, has_life BOOLEAN, 
 size INT NOT NULL, been_explored BOOLEAN, star_id INT, CONSTRAINT fk_star FOREING KEY(star_id) REFERENCES star(star_id)); 
 
-postgres=> CREATE TABLE moon (); 
+postgres=> CREATE TABLE moon (moon_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, distance_from_planet NUMERIC, size INT,
+planet_id INT, CONSTRAINT fk_planet FOREING KEY(planet_id) REFERENCES planet(planet_id)); 
 
+postgres=> CREATE TABLE black_hole(black_hole_id SERIAL PRIMARY KEY NOT NULL, name VARCHAR(50) NOT NULL, age_in million_years NUMERIC, distance_from_earth NUMERIC,
+galaxy_id INT, CONSTRAINT fk_galaxy FOREING KEY(galaxy_id) REFERENCES galaxy(galaxy_id)); 
